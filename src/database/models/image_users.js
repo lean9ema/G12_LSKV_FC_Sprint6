bd439a7902f2949023c6a29f5db5568684aa391b
lean9ema <lean9marino@gmail.com>
2022-02-09 19:31:53 -0300

    Lean- creacion de tablas de grupo 2 y 4

---
+++ src/database/models/image_users.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Image_users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Image_users.init({
+    url_name: DataTypes.STRING,
+    idUsers: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Image_users',
+  });
+  return Image_users;
+};
